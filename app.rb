7561161c6aa61363dbd8bd511c5a7c3b056075b6
Adam S <adam.sarli@gmail.com>
2019-05-05 00:00:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,8 @@ class App < Sinatra::Base
   end 
   
   post '/puppy' do 
-    puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    binding.pry 
     erb :display_puppy
   end 
 end
